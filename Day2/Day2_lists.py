ef322405f14f6a529ba01eee85fded21e421b8f0
Swati1910 <65558812+Swati1910@users.noreply.github.com>
2021-01-21 17:53:30 +0530

    Add files via upload

---
+++ Day2/Day2_lists.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    N = int(input())
+    L = []
+
+for i in range(0,N):
+    x = input().split()
+    if x[0] == "insert":
+        L.insert(int(x[1]), int(x[2]))
+    elif x[0] == "remove":
+        L.remove(int(x[1]))
+    elif x[0] == "append":
+        L.append(int(x[1]))
+    elif x[0] == "sort":
+        L.sort()
+    elif x[0] == "reverse":
+        L.reverse()
+    elif x[0] == "pop":
+        L.pop()
+    elif x[0] == "print":
+        print(L)
